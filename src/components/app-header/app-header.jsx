58276f051d6724f4e399b89cdfed10812bbb2720
maximle <mletunovskiy@yandex.ru>
2023-05-04 21:01:47 +0300

    header modified

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import styles from './app-header.module.css';
 import {Logo} from '@ya.praktikum/react-developer-burger-ui-components';
+import NavItem from '../nav-item/nav-item'
+
 
 class AppHeader extends React.Component {
   render() {
     return (
       <header className={`${styles.header}`}>
-        <p>test</p>  
-        <Logo />
+        <nav className={`${styles.nav}`}>
+          <ul className={`${styles.navItems}`}>
+            <li className={`${styles.navItem}`}>
+              <NavItem />
+            </li>
+            <li className={`${styles.navItem}`}>
+
+            </li>
+          </ul>
+        </nav>
+
+        <div className={`${styles.logo}`}>
+          <Logo />
+        </div>
+        
       </header>
     )
   }
---
+++ src/components/nav-item/nav-item.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './nav-item.module.css';
+
+
+class NavItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+
+  render() {
+    return(
+      <p>test</p>
+    )
+  }
+}
+
+export default NavItem;
